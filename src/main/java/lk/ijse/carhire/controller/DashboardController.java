0919135c4903d9e94483747097cdfee5905df4d6
bhanuka-viraj <bhanukaviraj22@gmail.com>
2023-10-25 14:20:29 +0530

    Dashboard created

--- src/main/java/lk/ijse/carhire/controller/DashboardController.java
+++ src/main/java/lk/ijse/carhire/controller/DashboardController.java
@@ -18,6 +18,13 @@ public class DashboardController {
     private AnchorPane rootnode;
 
 
+    public void initialize(){
+        try {
+            loadFXML("/lk/ijse/carhire/view/rent/rent_list.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @FXML
     void btnCustListOnClick(ActionEvent event) throws IOException {
     loadFXML("/lk/ijse/carhire/view/customer/customer_list.fxml");
@@ -69,14 +76,22 @@ public class DashboardController {
         Stage stage=new Stage();
 
         stage.setScene(scene);
-        stage.setTitle("Customer Form");
+        stage.setTitle("Car Form");
         stage.centerOnScreen();
         stage.show();
     }
 
     @FXML
-    void btnCategoriesOnClick(ActionEvent event) {
+    void btnCategoriesOnClick(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("/lk/ijse/carhire/view/car/car_category.fxml"));
+        Scene scene=new Scene(root);
 
+        Stage stage=new Stage();
+
+        stage.setScene(scene);
+        stage.setTitle("Car Categories");
+        stage.centerOnScreen();
+        stage.show();
     }
 
     @FXML
--- src/main/java/lk/ijse/carhire/controller/car/CarCategoryController.java
+++ src/main/java/lk/ijse/carhire/controller/car/CarCategoryController.java
@@ -1,4 +1,69 @@
 package lk.ijse.carhire.controller.car;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CarCategoryController {
+
+    @FXML
+    private TableColumn<?, ?> ColCategory;
+
+    @FXML
+    private Label lblHead1;
+
+    @FXML
+    private TableColumn<?, ?> ColSelect;
+
+    @FXML
+    private TableView<?> tblCategories;
+
+    @FXML
+    void btnSedanOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSuvOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHatchBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConvertibleonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddNewCategoryOnAction(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("/lk/ijse/carhire/view/car/category_form.fxml"));
+        Scene scene=new Scene(root);
+
+        Stage stage=new Stage();
+
+        stage.setScene(scene);
+        stage.setTitle("Car Categories");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+
+
 }
